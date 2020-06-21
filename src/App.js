d86ee4e16f419d83376fca123d4642c7e447826b
unknown <yunusyerli1@gmail.com>
2020-06-21 16:16:18 +0300

    beginning of ecommerce website

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="App-header">
+        sad
+      </header>
+      <main>asd</main>
+      <footer>asd</footer>
+    </div>
+  );
+}
+
+export default App;
