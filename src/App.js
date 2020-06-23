b1229c7892d62f825073e7b479cb4a9e5d7da35e
unknown <yunusyerli1@gmail.com>
2020-06-23 23:58:51 +0300

    Product Items Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import data from './data.json';
+import Products from './components/Products'
 
 
-function App() {
-  return (
+class App extends React.Component {
+  state={
+    products: data.products,
+    size:"",
+    sort:""
+  };
+  
+  render(){
+    console.log(this.state.products);
+    return (
     <div className="grid-container">
       <header className="App-header">
-        sad
+        <a href="/">Shopping Cart</a>
       </header>
-      <main>asd</main>
-      <footer>asd</footer>
+      <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">
+              Sidebar
+            </div>
+          </div>
+      </main>
+      <footer>All right reserved.</footer>
     </div>
   );
 }
-
+}
 export default App;
