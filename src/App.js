a40d79cd6a4a33e7bb22e9d6361d292970e34817
unknown <yunusyerli1@gmail.com>
2020-06-26 22:43:38 +0300

    cart-component-Effect-Modal

--- src/App.js
+++ src/App.js
@@ -2,14 +2,41 @@ import React from 'react';
 import data from './data.json';
 import Products from './components/Products'
 import Filter from './components/Filter';
-
+import Cart from './components/Cart';
 
 class App extends React.Component {
   state={
     products: data.products,
+    cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
     size:"",
     sort:""
   };
+  createOrder=(order)=>{
+    //
+  }
+  removeFromCart = product => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter(x => x._id !== product._id) 
+    });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter(x => x._id !== product._id)));
+  }
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if(item._id === product._id){
+        item.count++;
+        alreadyInCart=true;
+      }
+      
+    })
+    if(!alreadyInCart){
+      cartItems.push({...product, count:1});
+    }
+    this.setState({cartItems});
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }
   sortProducts = (e) => {
     const sort = e.target.value;
     this.setState({
@@ -42,6 +69,7 @@ class App extends React.Component {
   }
   
   render(){
+
     return (
     <div className="grid-container">
       <header className="App-header">
@@ -57,10 +85,15 @@ class App extends React.Component {
                   filterProducts={this.filterProducts}
                   sortProducts={this.sortProducts}
               />
-              <Products products={this.state.products} />
+              <Products 
+                  products={this.state.products} 
+                  addToCart={this.addToCart}/>
             </div>
             <div className="sidebar">
-              Sidebar
+              <Cart 
+                  cartItems={this.state.cartItems}
+                  removeFromCart= {this.removeFromCart}
+                  createOrder={this.createOrder}/>
             </div>
           </div>
       </main>
