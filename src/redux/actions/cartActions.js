bad2c2f05f5b9dbdbeef5cc77c08e401d0756447
unknown <yunusyerli1@gmail.com>
2020-07-11 12:33:33 +0300

    create-order

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -23,13 +23,10 @@ export const addToCart = (product) =>  (dispatch, getState) => {
 
 
 
-export const removeFromCart = (product) =>  (dispatch, getState) => {
-    const cartItems = getState().cart.cartItems.slice().filter(
-        x => x._id !== product._id
-    );
-    dispatch({
-        type: REMOVE_FROM_CART,
-        payload: {cartItems}
-    });
-    localStorage.setItem("cartItems",JSON.stringify(cartItems));
-};
+export const removeFromCart = (product) => (dispatch, getState) => {
+    const cartItems = getState()
+      .cart.cartItems.slice()
+      .filter((x) => x._id !== product._id);
+    dispatch({ type: REMOVE_FROM_CART, payload: { cartItems } });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  };
